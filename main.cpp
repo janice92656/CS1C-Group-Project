1004706c2f7c0d9df06a4bfe90cdfeb91d84946e
janice92656 <janice92656@users.noreply.github.com>
2014-10-27 17:12:52 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/****************************************************************
+* AUTHOR		: Janice Wallace, Kyle Serio, Matt, Seth Tyler
+* ASSIGNMENT            : Class Project 
+* CLASS			: CS1C
+* SECTION		: TTh: 6-7:30pm
+* DUE DATE:		: December 9, 2014
+****************************************************************/
+
+#include <iostream>	
+using namespace std;
+
+int main()
+{
+	cout << "Programmed by	: Janice Wallace, Kyle Serio, Seth Tyler, Matt \n";
+	cout << "CS1B		: TTh 6-7:30 pm\n";
+	cout << "Assignment	: Class Project\n\n";
+
+	return 0;
+}
