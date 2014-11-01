05aa190126ba161bf08d50e518f80cf4af2d3667
janice92656 <janice92656@users.noreply.github.com>
2014-11-01 16:08:13 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,16 +13,24 @@ using namespace std;
 
 // void readFromFile(string& fileName);
 void readPricing();
+void readGuarantee();
 
 int main()
 {
 	cout << "Programmed by	: Janice Wallace, Kyle Serio, Seth Tyler, Matt \n";
 	cout << "CS1B		: TTh 6-7:30 pm\n";
 	cout << "Assignment	: Class Project\n\n";
-        string fileName;
-        readPricing();
-        fileName = "pricing.txt";
+        
+        readPricing();      //  These two function calls will be part of the 
+        cout << endl;
+        readGuarantee();    // menu once it is created
+        
+        
+        // string fileName;
+        // fileName = "pricing.txt";
         // readFromFile(fileName);
+        // readFromFile("pricing.txt");
+        
 	return 0;
 }
 
@@ -30,6 +38,20 @@ void readPricing()
 {
     string nextLine;
     ifstream inData("pricing.txt");
+    if (inData.is_open())
+    {
+        while (getline(inData, nextLine))
+            cout << nextLine << endl;
+    }
+    else
+        cout << "Unable to open file.";
+    inData.close();
+}
+
+void readGuarantee()
+{
+    string nextLine;
+    ifstream inData("guarantee.txt");
     if (inData.is_open())
     {
         while (getline(inData, nextLine))
