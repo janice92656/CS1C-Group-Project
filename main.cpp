b06974022593ad095c0e603cf28919937bd65890
mregueiro0 <msregu@gmail.com>
2014-11-01 16:43:04 -0700

    Update main.cpp
    I added my last name in line 22. 
    I also added my code to read in the Terrain.txt and Operations.txt files,

--- main.cpp
+++ main.cpp
@@ -14,24 +14,58 @@ using namespace std;
 // void readFromFile(string& fileName);
 void readPricing();
 void readGuarantee();
+void readOperations();
+void readTerrains();
 
 int main()
 {
-	cout << "Programmed by	: Janice Wallace, Kyle Serio, Seth Tyler, Matt \n";
+	cout << "Programmed by	: Janice Wallace, Kyle Serio, Seth Tyler, Matt Regueiro\n";
 	cout << "CS1B		: TTh 6-7:30 pm\n";
 	cout << "Assignment	: Class Project\n\n";
-        
-        readPricing();      //  These two function calls will be part of the 
-        cout << endl;
-        readGuarantee();    // menu once it is created
-        
-        
+
         // string fileName;
         // fileName = "pricing.txt";
         // readFromFile(fileName);
         // readFromFile("pricing.txt");
         
+        readPricing();      //  These two function calls will be part of the 
+        cout << endl;
+        readGuarantee();    // menu once it is created
+        cout << endl;
+        readOperations();   // Will display the order of operations. Will be part
+        		    // of the main menu once it has been created.
+       	cout << endl;
+       	readTerrains();	    // Will display the physical terrains. Will be part
+       			    // of the main menu once it has been created.
+        cout << endl;
+        
 	return 0;
+}
+void readOperations()
+{
+	ifstream inFile;
+	string sentence;
+	inFile.open("Operations.txt");
+	do
+	{
+		getline(inFile, sentence);
+		cout << sentence << endl;
+	}while(inFile);
+	inFile.close();
+}
+
+void readTerrain()
+{
+	ifstream inFile;
+	string sentence;
+	inFile.open("Terrain.txt");
+	do
+	{
+		getline(inFile, sentence);
+		cout << sentence << endl;
+	}while(inFile);
+	inFile.close();
+	
 }
 
 void readPricing()
