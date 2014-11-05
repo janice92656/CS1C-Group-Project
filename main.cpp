62568d1d3fb1ecce9ff02bef119eb2854c62a6bd
kserio0 <k67serio54@gmail.com>
2014-11-04 20:17:12 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,104 +11,84 @@
 #include <string>
 using namespace std;
 
-// void readFromFile(string& fileName);
-void readPricing();
-void readGuarantee();
-void readOperations();
-void readTerrains();
+ void ReadFile (string fileName);
+ string MenuFuncOne();
 
 int main()
 {
 	cout << "Programmed by	: Janice Wallace, Kyle Serio, Seth Tyler, Matt Regueiro\n";
 	cout << "CS1B		: TTh 6-7:30 pm\n";
 	cout << "Assignment	: Class Project\n\n";
+	
+	int choice;
+	
+	cout << MenuFuncOne();
+	cin >> choice;
+	cin.ignore(1000,'\n');
+	
+	while(choice > 0)
+	{
 
-        // string fileName;
-        // fileName = "pricing.txt";
-        // readFromFile(fileName);
-        // readFromFile("pricing.txt");
-        
-        readPricing();      //  These two function calls will be part of the 
-        cout << endl;
-        readGuarantee();    // menu once it is created
-        cout << endl;
-        readOperations();   // Will display the order of operations. Will be part
-        		    // of the main menu once it has been created.
-       	cout << endl;
-       	readTerrains();	    // Will display the physical terrains. Will be part
-       			    // of the main menu once it has been created.
-        cout << endl;
+		
+		switch (choice)
+		{
+			case 1 : ReadFile("Terrain.txt");
+			break;
+			case 2 : ReadFile("Operations.txt");
+			break;
+			case 3 : ReadFile("Guarantee.txt");
+			break;
+			case 4 : ReadFile("Pricing.txt");
+			break;
+			case 5 : ReadFile("Logistics.txt");
+			break;
+			case 5 : ReadFile("Testimonies.txt");
+			break;
+			default : cout << "Error, Incorrect Input!\n";
+			
+		}
+		
+	cout << MenuFuncOne();
+	cin >> choice;
+	cin.ignore(1000,'\n');
+		
+	}
         
 	return 0;
 }
-void readOperations()
-{
-	ifstream inFile;
-	string sentence;
-	inFile.open("Operations.txt");
-	do
-	{
-		getline(inFile, sentence);
-		cout << sentence << endl;
-	}while(inFile);
-	inFile.close();
-}
 
-void readTerrain()
++///************************************************************************
++//* MenuFuncOne
++//* This function returns the first segment of the programs menu
++//* PRE-CONDITIONS
++// - NONE -
++//* POST- CONDITIONS
++//* menuSegment: The segment is stored and returned here.
++//* - RETURNS -> The menu segment.
++//*************************************************************************/
++string MenuFuncOne()
++{
++ string menuSegment = "\n1 - Selling Points"
+	+ "\n2 - Environments Supported"
+	+ "\n3 - Concept of operations"
+	+ "\n4 - Guarantee Policy"
+	+ "\n5 - Options and Pricing"
+	+ "\n6 - Logistics and Maintenance"
+	+ "\n7 - Customer Testimonies";
++ return menuSegment;
++}
+
+void ReadFile(string fileName)
 {
-	ifstream inFile;
-	string sentence;
-	inFile.open("Terrain.txt");
-	do
-	{
-		getline(inFile, sentence);
-		cout << sentence << endl;
-	}while(inFile);
-	inFile.close();
+	ifstream iFile;
+	string store;
+	iFile.open(fileName.c_str());
 	
+	while(iFile)
+	{
+		iFile >> store;
+		cout << store;
+	}
+	iFile.close();
 }
 
-void readPricing()
-{
-    string nextLine;
-    ifstream inData("pricing.txt");
-    if (inData.is_open())
-    {
-        while (getline(inData, nextLine))
-            cout << nextLine << endl;
-    }
-    else
-        cout << "Unable to open file.";
-    inData.close();
-}
-
-void readGuarantee()
-{
-    string nextLine;
-    ifstream inData("guarantee.txt");
-    if (inData.is_open())
-    {
-        while (getline(inData, nextLine))
-            cout << nextLine << endl;
-    }
-    else
-        cout << "Unable to open file.";
-    inData.close();
-}
-
-/*
-void readFromFile(string& fileName)
-{
-    cout << "readFromFile called.";
-    string nextLine;
-    ifstream inData(fileName);
-    if (inData.is_open())
-    {
-        while (getline(inData, nextLine))
-            cout << nextLine << endl;
-    }
-    else
-        cout << "Unable to open file.";
-    inData.close();
-}
-*/
