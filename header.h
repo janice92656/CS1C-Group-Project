efd81f2565eee6a5d9e0364bb77040b1cea5048f
janice92656 <janice92656@users.noreply.github.com>
2014-12-02 23:36:14 -0800

    Create header.h

---
+++ header.h
@@ -0,0 +1,14 @@
+#include <string>
+using namespace std;
+
+bool LogIn();
+void RequestBrochure();
+void ReadFile (string fileName);
+string MenuFuncOne();
+string MaintenanceFunc();
+void CustomerMenu();
+void EmployeeMenu();
+int WelcomeMenu();
+void AddUser(string fileName);
+void RemoveUser(string fileName);
+void PrintBrochure();
